34575f73f4685ecf776cd2e4e38677a0072199d2
ruanndias <ruan_ndias@hotmail.com>
2020-09-04 11:37:21 -0300

    showing beer on screen from API

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const inputSearch = document.getElementById('inputSearch');
-console.log(inputSearch);
+const inputSearch = document.getElementById('inputSearch')
+
+const container = document.getElementById('container')
+container.setAttribute('class', 'container')
+
 
 async function getBeers() 
 {
-  let response = await fetch(`https://api.punkapi.com/v2/beers`);
+  let response = await fetch(`https://api.punkapi.com/v2/beers`)
   let data = await response.json()
-  return data;
+  data.forEach(function({ image_url, name, tagline }) {
+    console.log(customBeer(image_url, name, tagline))
+
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    const img = document.createElement('img')
+    img.src = image_url
+
+    const h1 = document.createElement('h1')
+    h1.textContent = name
+
+    const p = document.createElement('p')
+    p.textContent = tagline
+
+
+    container.appendChild(card)
+    card.appendChild(img)
+    card.appendChild(h1)
+    card.appendChild(p)
+  })
 }
 
-console.log(getBeers());
+getBeers()
+
+
+
+
+function customBeer(image_url, name, tagline) {
+  const beer = {
+    image: image_url,
+    name: name,
+    tagline: tagline 
+  }
+  return beer
+}
