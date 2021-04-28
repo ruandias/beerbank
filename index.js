d4e1ad8f55c90fcad718fddb3626adfc62f0c609
ruandias <ruan_ndias@hotmail.com>
2021-04-28 16:12:01 -0300

    showing beers on cards - FlexBox

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const inputSearch = document.getElementById('inputSearch')
 const container = document.getElementById('container')
 container.setAttribute('class', 'container')
 
-
 async function getBeers() 
 {
   let response = await fetch(`https://api.punkapi.com/v2/beers`)
   let data = await response.json()
+  
   data.forEach(function({ image_url, name, tagline }) {
-    console.log(customBeer(image_url, name, tagline))
 
     const card = document.createElement('div')
     card.setAttribute('class', 'card')
@@ -17,17 +16,21 @@ async function getBeers()
     const img = document.createElement('img')
     img.src = image_url
 
+    const cardText = document.createElement('div')
+    cardText.setAttribute('class', 'cardText')
+
     const h1 = document.createElement('h1')
     h1.textContent = name
 
     const p = document.createElement('p')
     p.textContent = tagline
 
-
+    
     container.appendChild(card)
     card.appendChild(img)
-    card.appendChild(h1)
-    card.appendChild(p)
+    card.appendChild(cardText)
+    cardText.appendChild(h1)
+    cardText.appendChild(p)
   })
 }
 
@@ -35,12 +38,3 @@ getBeers()
 
 
 
-
-function customBeer(image_url, name, tagline) {
-  const beer = {
-    image: image_url,
-    name: name,
-    tagline: tagline 
-  }
-  return beer
-}
